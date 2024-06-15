142131cd9585c9c4b2fc4d89e849d84aff99d8e4
m-khanddorj <m.khanddorj@gmail.com>
2024-06-15 20:51:28 +0900

    Updates

---
+++ precache-manifest.b63692c5907b4949461780f375b43fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118eb5f4bdc55f6d114e581592458c2f",
+    "url": "/nobs_vocab_builder/index.html"
+  },
+  {
+    "revision": "b3448a097eeac2a82f78",
+    "url": "/nobs_vocab_builder/static/css/main.52966856.chunk.css"
+  },
+  {
+    "revision": "5cc4a54f9d3042c43368",
+    "url": "/nobs_vocab_builder/static/js/2.6b4056ac.chunk.js"
+  },
+  {
+    "revision": "7eee806e32334195d38d",
+    "url": "/nobs_vocab_builder/static/js/3.cea1fcbc.chunk.js"
+  },
+  {
+    "revision": "b3448a097eeac2a82f78",
+    "url": "/nobs_vocab_builder/static/js/main.9bdf5801.chunk.js"
+  },
+  {
+    "revision": "a7266a0c39576fc68ce4",
+    "url": "/nobs_vocab_builder/static/js/runtime~main.e17c8110.js"
+  }
+]);
